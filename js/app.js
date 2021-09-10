3008d12ce9d7c5bad253b4f359718d3d3558d7f9
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2021-09-10 13:03:13 +0200

    miner change

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+/* for (let i = 0; i < navn.sidderKamarater.length; i++) {
+	console.log(navn.sidderKamarater[i].fornavn);
+	console.log(navn.sidderKamarater[i].efternavn);
+} */
+//forEach if we have an array, it could be good to use forEACH instead of forloop!
+/* sidderKamarater.navn.forEach((element) => {
+	console.log(element);
+}); */
+/* .catch(function (err) {
+					console.log(err);
+				}) */
+/* function appendData(param) {
+	let getMyData = document.querySelector("#myData ul");
+	for (let i = 0; i < navn.sidderKamarater.length; i++) {
+		let div = document.createElement("li");
+		div.innerHTML = `${param}: ${navn.sidderKamarater[i][param]} `;
+		getMyData.appendChild(div);
+	}
+}
+appendData("fornavn");
+appendData("efternavn");
+appendData("alder");
+appendData("hårfarve");
+appendData("skostørrelse");
+ */
+
 //fetch promis base
 fetch("./team.json")
 	.then((response) => response.json())
@@ -17,9 +43,3 @@ fetch("./team.json")
 		appendData("hårfarve");
 		appendData("skostørrelse");
 	});
-
-//forEach
-//if we have an array, it could be good to use forEACH instead of forloop!
-/* sidderKamarater.navn.forEach((element) => {
-	console.log(element);
-}); */
