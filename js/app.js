7d5e8f71de54a957a4bc21e26bc1c402453793ce
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2021-09-10 13:01:45 +0200

    fetch api done

--- js/app.js
+++ js/app.js
@@ -1,28 +1,25 @@
 //fetch promis base
-fetch("team.json").then((response) => console.log(response.json()));
+fetch("./team.json")
+	.then((response) => response.json())
+	.then((data) => {
+		/* console.log(data); */
+		function appendData(param) {
+			let getMyData = document.querySelector("#myData ul");
+			for (let i = 0; i < data.sidderKamarater.length; i++) {
+				let div = document.createElement("li");
+				div.innerHTML = `${param}: ${data.sidderKamarater[i][param]} `;
+				getMyData.appendChild(div);
+			}
+		}
+		appendData("fornavn");
+		appendData("efternavn");
+		appendData("alder");
+		appendData("hårfarve");
+		appendData("skostørrelse");
+	});
 
-/* for (let i = 0; i < navn.sidderKamarater.length; i++) {
-	console.log(navn.sidderKamarater[i].fornavn);
-	console.log(navn.sidderKamarater[i].efternavn);
-} */
-//forEach if we have an array, it could be good to use forEACH instead of forloop!
+//forEach
+//if we have an array, it could be good to use forEACH instead of forloop!
 /* sidderKamarater.navn.forEach((element) => {
 	console.log(element);
 }); */
-/* .catch(function (err) {
-					console.log(err);
-				}) */
-/* function appendData(param) {
-	let getMyData = document.querySelector("#myData ul");
-	for (let i = 0; i < navn.sidderKamarater.length; i++) {
-		let div = document.createElement("li");
-		div.innerHTML = `${param}: ${navn.sidderKamarater[i][param]} `;
-		getMyData.appendChild(div);
-	}
-}
-appendData("fornavn");
-appendData("efternavn");
-appendData("alder");
-appendData("hårfarve");
-appendData("skostørrelse");
- */
